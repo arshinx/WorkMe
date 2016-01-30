526f6f70444095a6e5b625a77840c9aa644bde5f
Janesh Chhabra <Janesh Chhabra>
2016-01-30 02:16:45 -0800

    implemented user authentication

--- loginpage.js
+++ loginpage.js
@@ -1,15 +1,20 @@
-$(document).ready(function){
+$(document).ready(function(){
   var ref = new Firebase("https://workme.firebaseio.com/");
-  ref.authWithPassword({
-    email    : "bobtony@firebase.com",
-    password : "correcthorsebatterystaple"
-   }, function(error, authData) {
+   console.log(" dsu javascript");
+
+  $("#submit").click(function() {
+    var sub_email = $("#email").val();
+    var sub_password = $("#password").val();
+    ref.authWithPassword({
+        email    : sub_email,
+        password : sub_password
+    }, function(error, authData) {
         if (error) {
-          console.log("Login Failed!", error);
-        }else {
-          console.log("Authenticated successfully with payload:", authData);
-        }
+        console.log("Login Failed!", error);
+        } else {
+        console.log("Authenticated successfully with payload:", authData);
+  }
 });
 
-
-}
+});
+});
