5d7b91a693660d7c661446af21c14b0291846e0d
Janesh Chhabra <Janesh Chhabra>
2016-01-30 19:02:54 -0800

    getting coordinates works now

--- location.js
+++ location.js
@@ -12,8 +12,25 @@ $(document).ready(function(){
    // Register the callback to be fired every time auth state changes
    var ref = new Firebase("https://workme.firebaseio.com");
    ref.onAuth(authDataCallback);
+   var coordinate;
+   var address_full;
+   var address;
    var authData = ref.getAuth();
    var url = "https://workme.firebaseio.com/users/"+authData.uid+"/full_address";
-   var  = new Firebase(url).once('value', function(snap){
-   console.log(url);
+   var address_funct = new Firebase(url).once('value', function(snap){
+      address_full=snap.val();
+      var temp=address_full.split(" ");
+      address = temp[0];
+      var i;
+      for (i=1; i<temp.length-1; i++){
+         address+= "+";
+         address += temp[i];
+      }
+      address += temp[temp.length-1];
+      console.log(address);
+      $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + address, function(data, textStatus){
+         coordinate = data.results[0].geometry.location;
+         console.log(coordinate);
+      });
+});
 });
