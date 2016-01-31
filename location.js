f3363f9c89d074f8eb34f01ff50536857f742c97
Christopher Bui <Christopher Bui>
2016-01-30 20:01:11 -0800

    implementing location in register

--- location.js
+++ location.js
@@ -12,7 +12,8 @@ $(document).ready(function(){
    // Register the callback to be fired every time auth state changes
    var ref = new Firebase("https://workme.firebaseio.com");
    ref.onAuth(authDataCallback);
-   var coordinate;
+   var object_coordinate;
+   var coordinate
    var address_full;
    var address;
    var authData = ref.getAuth();
@@ -29,8 +30,32 @@ $(document).ready(function(){
       address += temp[temp.length-1];
       console.log(address);
       $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=" + address, function(data, textStatus){
-         coordinate = data.results[0].geometry.location;
-         console.log(coordinate);
+         object_coordinate = data.results[0].geometry.location;
+         console.log(object_coordinate);
+         //console.log(coordinate[0]);
       });
+      
 });
+      var map;
+      function initialize() {
+        var mapOptions = {
+          zoom: 2,
+          center: {lat: -33.865427, lng: 151.196123},
+          mapTypeId: google.maps.MapTypeId.TERRAIN
+        };
+        map = new google.maps.Map(document.getElementById('map'),
+              mapOptions);
+        // Create a <script> tag and set the USGS URL as the source.
+        var script = document.createElement('script');
+        // (In this example we use a locally stored copy instead.)
+        // script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
+        //script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
+        document.getElementsByTagName('head')[0].appendChild(script);
+      }
+      function eqfeed_callback(results) {
+        map.data.addGeoJson(results);
+      }
+      // Call the initialize function after the page has finished loading
+      google.maps.event.addDomListener(window, 'load', initialize);
+
 });
